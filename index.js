3e84dff69fb2f8e95da3e477841351ff2a9cc80b
mighty_yaroslav <yaroslav@nomadhomes.co>
2024-02-06 18:46:55 +0200

    basic simpleParser version

--- index.js
+++ index.js
@@ -1,70 +1,16 @@
 const fs = require('fs');
-const util = require('util');
-const { MailParser } = require('mailparser');
+const { simpleParser } = require('mailparser');
 const EmailReplyParser = require("email-reply-parser");
 
-let emailParser = new MailParser({
-  // skipTextLinks: true,
-  skipHtmlToText: true, skipTextToHtml: true, skipImageLinks: true,
-});
-
 const main = async () => {
-  const filestream = fs.createReadStream('emails/gmail-large-text-with-pdf.eml', { encoding: 'utf-8' });
+  const filestream = fs.createReadStream('emails/gmail-large-text-with-pdf.eml', { encoding: 'utf-8', highWaterMark: 1000 });
+  const parsedEmail = await simpleParser(filestream, { skipHtmlToText: true, skipTextToHtml: true, skipImageLinks: true })
 
-  filestream.pipe(emailParser);
+  const email = new EmailReplyParser().read(parsedEmail.text);
 
-  const emailText = await new Promise((resolve, reject) => {
-    emailParser.on('headers', headers => {
-      console.log(util.inspect(headers, false, 22));
-    });
-
-    let filedata = '';
-    let i = 0;
-    emailParser.on('data', (data) => {
-      if (data.type === 'text') {
-        filedata += data.text
-        console.log(data.text)
-        i++;
-        console.log('Email text chunk ' + i + ' was received!!\n\n\n\n')
-        // if (filedata.length > 500) {
-        //   filestream.destroy()
-        //   return;
-        // }
-      }
-
-    })
-
-    emailParser.on('error', (err) => {
-      console.error(err)
-      reject(err)
-    })
-
-    emailParser.on('end', () => {
-      console.info('Email stream ended');
-      resolve(filedata);
-    })
-    emailParser.on('close', () => console.info('Email stream closed'))
-  })
-
-  // const parsedEmail = await new Promise((resolve, reject) => {
-  //   fs.readFile('emails/gmail-large-text.eml', 'utf-8', (err, data) => {
-  //     if (err) reject(err);
-  //     simpleParser(data, { skipTextLinks: true, skipHtmlToText: true }, (err, parsedEmail) => {
-  //       if (err) reject(err);
-  //       resolve(parsedEmail);
-  //     })
-  //   })
-  // })
-
-  // const email = new EmailReplyParser().read(parsedEmail.text);
-
-  console.log(emailText)
-  const email = new EmailReplyParser().read(emailText);
-
-  console.log(emailText)
+  console.log(email.getVisibleText())
 
   fs.writeFileSync('results/gmail-large-text-with-pdf.txt', email.getVisibleText())
-
 }
 
 
