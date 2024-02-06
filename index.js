35bc6596b8db6b61b0626b0d939658b19f123c3b
mighty_yaroslav <yaroslav@nomadhomes.co>
2024-02-06 18:58:06 +0200

    simpleParser with stream limit

--- index.js
+++ index.js
@@ -4,7 +4,39 @@ const EmailReplyParser = require("email-reply-parser");
 
 const main = async () => {
   const filestream = fs.createReadStream('emails/gmail-large-text-with-pdf.eml', { encoding: 'utf-8', highWaterMark: 1000 });
-  const parsedEmail = await simpleParser(filestream, { skipHtmlToText: true, skipTextToHtml: true, skipImageLinks: true })
+
+  const emailContent = await new Promise((resolve, reject) => {
+    let filedata = '';
+    let i = 0;
+    filestream.on('data', (data) => {
+        filedata += data
+        console.log(data)
+        i++;
+        console.log('Email text chunk ' + i + ' was received!!\n\n\n\n')
+        if (filedata.length > 1_000_000) {
+          filestream.destroy()
+          return;
+        }
+
+    })
+
+    filestream.on('error', (err) => {
+      console.error(err)
+      reject(err)
+    })
+
+    filestream.on('end', () => {
+      console.info('Email stream ended');
+      resolve(filedata);
+    })
+    filestream.on('close', () => {
+      console.info('Email stream closed')
+      resolve(filedata);
+    })
+  })
+
+
+  const parsedEmail = await simpleParser(emailContent, { skipHtmlToText: true, skipTextToHtml: true, skipImageLinks: true })
 
   const email = new EmailReplyParser().read(parsedEmail.text);
 
